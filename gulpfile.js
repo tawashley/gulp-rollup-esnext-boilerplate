c90724e697dbd1b20961e19ed242a88101300bca
tawashley <trisashley@gmail.com>
2018-04-04 10:15:49 +0100

    feat: Add babel rollup, babel config. Compile main.js and main-legacy.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,48 @@
+var babel = require('rollup-plugin-babel');
+var del = require('del');
+
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var rollup = require('gulp-better-rollup');
 
+function getRollupConfig({ isLegacy = false } = {}) {
+    var options = {};
+
+    if(isLegacy === true) {
+        options.plugins = [
+            babel({
+                exclude: 'node_modules/**'
+            })
+        ]
+    }
+
+    return options;
+}
+
+gulp.task('clean', function () {
+    return del(['./dist'], {
+        force: true
+    });
+});
+
 gulp.task('scripts:compile', function() {
-    return gulp.src('./src/scripts/entry.js')
+    return gulp.src('./src/scripts/main.js')
         .pipe(sourcemaps.init())
-        .pipe(rollup({},  {
-            format: 'cjs',
+        .pipe(rollup(getRollupConfig(), {
+            format: 'es'
         }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('default', gulp.series('scripts:compile'))
+gulp.task('scripts:compile-legacy', function() {
+    return gulp.src('./src/scripts/main-legacy.js')
+        .pipe(sourcemaps.init())
+        .pipe(rollup(getRollupConfig({ isLegacy: true}), {
+            format: 'es'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('default', gulp.series('clean', 'scripts:compile', 'scripts:compile-legacy'))
