0e94e37f3df27bcebecadf5ec38db57e0b9e79cc
tawashley <trisashley@gmail.com>
2018-04-04 12:48:28 +0100

    feat: Move babel config management into gulp, target different set of browsers depending on if legacy JS bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var babel = require('rollup-plugin-babel');
+// var babel = require('rollup-plugin-babel');
 var filesize = require('rollup-plugin-filesize');
 var del = require('del');
 var argv = require('yargs').argv;
+var babel = require('gulp-babel');
 
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
@@ -18,13 +19,43 @@ function getRollupConfig({ isLegacy = false } = {}) {
         filesize()
     ];
 
+    return options;
+}
+
+function getBabelBrowsersConfig(isLegacy = false) {
+    var browsersList = []
+
     if(isLegacy === true) {
-        options.plugins.push(
-            babel({
-                exclude: 'node_modules/**'
-            })
+        browsersList.push(
+            '> 1%',
+            'last 2 versions',
+            'Firefox ESR'
+        );
+    } else {
+        browsersList.push(
+            'Chrome >= 60',
+            'Safari >= 10.1',
+            'iOS >= 10.3',
+            'Firefox >= 54',
+            'Edge >= 15'
         );
     }
+
+    return {
+        browsers: browsersList
+    };
+}
+
+function getBabelConfig({ isLegacy = false } = {}) {
+    var options = {
+        babelrc: false,
+        presets:  [
+            ["@babel/env", {
+                modules: false,
+                targets: getBabelBrowsersConfig(isLegacy)
+            }]
+        ]
+    };
 
     return options;
 }
@@ -43,6 +74,7 @@ gulp.task('scripts:main', function() {
     return gulp.src('./_source/scripts/main.js')
         .pipe(gulpif(!isProd, sourcemaps.init()))
         .pipe(rollup(getRollupConfig(), ROLLUP_MODULE_FORMAT))
+        .pipe(babel(getBabelConfig()))
         .pipe(gulpif(isProd, uglify()))
         .pipe(gulpif(!isProd, sourcemaps.write()))
         .pipe(gulp.dest('./dist/scripts'))
@@ -52,6 +84,7 @@ gulp.task('scripts:main-legacy', function() {
     return gulp.src('./_source/scripts/main-legacy.js')
         .pipe(gulpif(!isProd, sourcemaps.init()))
         .pipe(rollup(getRollupConfig({ isLegacy: true}), ROLLUP_MODULE_FORMAT))
+        .pipe(babel(getBabelConfig({ isLegacy: true })))
         .pipe(gulpif(isProd, uglify()))
         .pipe(gulpif(!isProd, sourcemaps.write()))
         .pipe(gulp.dest('./dist/scripts'))
